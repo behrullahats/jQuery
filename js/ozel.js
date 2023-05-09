e64c3d4bc562e90a522e41d41515de1bfdc3bb2a
Seyit Behrullah Ateş <117732354+behrullahats@users.noreply.github.com>
2023-05-09 19:35:00 +0300

    jQuery Notları
    Ders 1

---
+++ js/ozel.js
@@ -0,0 +1,73 @@
+// $(document).ready(function () {
+//     $("p").click(function () {
+//         $(this).hide();
+//     });
+// });
+
+
+
+// // 1.Yönten
+// $(document).ready(function () {
+
+//     $('i').html("Seyit Behrullah Ateş");
+
+// });
+
+
+// // 2. Yöntem
+// $().ready(function () {
+
+//     $('b').html("22 dir");
+
+// });
+
+// //3.Yöntem
+// $(function () {
+//     $("b").html("Merhaba Nasılsın Bakalım İyi misin?");
+// });
+
+// $(document).ready(function () {
+
+//     $('small').html("Merhaba Ben Küçük Yazı olan Small Etiketiyim");
+
+// });
+
+// $(document).ready(function () {
+//     $('i').html("Merhaba Ben Eğik yazı olan İ Etiketiyim");
+// });
+
+// $(document).ready(function () {
+//     $('div').html("Merhaba Ben Kutucuk olan Div Elemanıyım");
+// });
+
+// $(document).ready(function () {
+//     $("footer").html("Merhaba Ben Sayfanın sonunda bulunan footer elemanıyım");
+// });
+// $(function () {
+//     $('nav').html("Merhaba Ben Sayfanın Başında bulunan Nav Elamanıyım");
+// });
+
+// $().ready(function () {
+//     $('nav').html("Merahab Ben SAYFANIN BAŞINDA BULUNAN NAV ELEMANIYIM");
+// });
+// $(document).ready(function () {
+
+//     $('nav').html("Merhaba ben Sayfanın Başında bulunan nav elemanıyım birinci yöntem");
+// });
+
+
+// function islem() {
+//     $('div').html("Bu elemanı çağırmak için butona tıkladın");
+// }
+
+// function kapama() {
+//     $('div').html("");
+// }
+
+// $('.acma').click(islem);
+// $('.kapama').click(kapama);
+
+
+function ackapa() {
+    $('.deneme').toggle();
+}
